d3a1eafb969354a6409ebc2c7a36ac4d4031989e
B.Viktor <benkovik@gmail.com>
2021-02-17 00:12:51 +0100

    Menu, Home, Heroes components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
