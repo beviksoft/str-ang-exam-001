181bea4d2c030d9f29b677b507fb6324c84452c7
B.Viktor <benkovik@gmail.com>
2021-02-17 03:15:40 +0100

    Routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+
+
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "heroes", component: HeroesComponent },
+  { path: "**", component: HomeComponent }
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +24,8 @@ import { HeroesComponent } from './components/heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes) //set routes
   ],
   providers: [],
   bootstrap: [AppComponent]
